469a0646c6565667f66e431158a12e61f08e80ce
Jayson Kadlecek <jayson@propelplm.com>
2019-12-27 18:21:22 -0800

    most work complete on popup

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(details => {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+	if(changeInfo.url.startsWith("https://docs.google.com")){
+		chrome.pageAction.show(tabId);
+	}
+
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab){
+
+    if(tab.url.startsWith("https://docs.google.com")){
+			chrome.pageAction.show(activeInfo.tabId);
+		}
+
+  });
+}); 
---
+++ app/scripts.babel/popup.js
@@ -0,0 +1,4 @@
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(function (response) {
+	console.log(response);
+});
+
